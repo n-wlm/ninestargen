50b05d3c50c801e33f02c40aab402c1671a67438
Naim <111665962+n-wlm@users.noreply.github.com>
2026-03-24 20:11:29 +0100

    v1.4.2
    updated favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,8 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon.ico" },
       { url: "/favicon.svg", type: "image/svg+xml" },
-      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
